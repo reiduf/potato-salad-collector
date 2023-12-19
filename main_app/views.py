cdada57b9f4c6345fbe9db4a377df578aba40528
reina-duf <reina.dufrene@gmail.com>
2023-12-19 16:07:47 -0700

    adds sample potato data, all potato salads url and view and template

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+potatoes = [
+    {'brand': 'Reser\'s Red Skin Potato Salad', 'description': 'The best there is', 'rating': 5},
+    {'brand': 'Lunardi\'s SF Potato Salad', 'description': 'Best store made p.s.', 'rating': 5},
+    {'brand': 'Signature Kitchen\'s Classic Potato Salad', 'description': 'Absolute trash', 'rating': 1},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def potatoes_index(request):
+    return render(request, 'potatoes/index.html', {
+        'potatoes': potatoes
+    })
