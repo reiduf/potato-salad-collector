1dd8f317a1d8458e6a106eb823c4da6ca447b548
reina-duf <reina.dufrene@gmail.com>
2023-12-20 20:17:37 -0700

    adds functionality and UI for editing a potato salad. adds delete button but not functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('potatoes/', views.potatoes_index, name='index'),
   path('potatoes/<int:potato_id>/', views.potatoes_detail, name='detail'),
-  path('potatoes/create/', views.PotatoCreate.as_view(), name='potatoes_create')
+  path('potatoes/create/', views.PotatoCreate.as_view(), name='potatoes_create'),
+  path('potatoes/<int:pk>/update', views.PotatoUpdate.as_view(), name='potatoes_update'),
+  path('potatoes/<int:pk>/delete', views.PotatoDelete.as_view(), name='potatoes_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Potato
 
 # Create your views here.
@@ -24,3 +24,11 @@ def potatoes_detail(request, potato_id):
 class PotatoCreate(CreateView):
     model = Potato
     fields = '__all__'
+
+class PotatoUpdate(UpdateView):
+    model = Potato
+    fields = '__all__'
+
+class PotatoDelete(DeleteView):
+    model = Potato
+    success_url = '/potatoes'
